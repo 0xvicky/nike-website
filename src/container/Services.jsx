7dc0dea98403b431ff73e85112eadc6c0afde307
vicky <tyagivivek528@gmail.com>
2023-09-05 12:43:22 +0530

    services section done

--- src/container/Services.jsx
+++ src/container/Services.jsx
@@ -1,5 +1,18 @@
+import {services} from "../constants";
+import ServiceCard from "../components/ServiceCard";
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <div className='flex justify-between max-container flex-wrap gap-9'>
+      {services.map(service => {
+        return (
+          <ServiceCard
+            key={service.label}
+            {...service}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default Services;
