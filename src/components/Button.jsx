301387bc2aa9337f18506d79990cce02de5030a2
vicky <tyagivivek528@gmail.com>
2023-09-05 12:19:26 +0530

    Super quality section is done

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,11 +5,13 @@ const Button = ({label, imgUrl}) => {
     <div>
       <button className='flex justify-center items-center gap-2 px-7 py-4 font-montserrat text-lg leading-none rounded-full text-white border-coral-red bg-coral-red'>
         {label}
-        <img
-          src={imgUrl}
-          alt='arrow-right'
-          className='ml-2 rounded-full w-5 h-5 cursor-pointer'
-        />
+        {imgUrl && (
+          <img
+            src={imgUrl}
+            alt='arrow-right'
+            className='ml-2 rounded-full w-5 h-5 cursor-pointer'
+          />
+        )}
       </button>
     </div>
   );
--- src/container/SuperQuality.jsx
+++ src/container/SuperQuality.jsx
@@ -1,5 +1,36 @@
+import {shoe8} from "../assets/images";
+import {Button} from "../components";
+
 const SuperQuality = () => {
-  return <div>SuperQuality</div>;
+  return (
+    <div className='flex justify-between items-center max-lg:flex-col max-container gap-10 w-full'>
+      <div className='flex flex-col flex-1'>
+        <h2 className='font-palanquin font-bold text-4xl capitalize lg:max-w-lg'>
+          We Provide You
+          <span className='text-coral-red'> Super </span>
+          <span className='text-coral-red'> Quality </span>
+          Shoes
+        </h2>
+        <p className='lg:max-w-lg info-text mt-4'>
+          Ensuring premium comfort and style, our meticulously crafted footwear is
+          designed to elevate your experience, providing you with unmatched quality,
+          innovation, and a touch of elegance.
+          <p className='mt-6 lg:max-w-lg info-text'>
+            Our dedication to detail and excellence ensures your satisfaction
+          </p>
+        </p>
+        <div className='mt-11'>
+          <Button label='View Details' />
+        </div>
+      </div>
+      <div className='flex flex-1 justify-center items-center'>
+        <img
+          src={shoe8}
+          alt=''
+        />
+      </div>
+    </div>
+  );
 };
 
 export default SuperQuality;
